7375662297bb5b67955cb19b8e923fa255ee97ef
Hector Eduardo Perez <elpibeperez@gmail.com>
2017-01-29 19:38:47 -0300

    Solving issue related to x y values

--- R/rImageVisualizer.R
+++ R/rImageVisualizer.R
@@ -2,30 +2,36 @@ library(tcltk2)
 library(rPython)
 
 
-
+SCRIPT_PATH =  "src/python/"
+SCRIPT_FILE_NAME = "image-generator-from-matrix.py"
+PACKAGE_NAME = "rImageVisualizer"
 
 
 rImageVisualizerVisualize = function(){
+
+  labelText <- tclVar("X:    Y:")
+
   generating_image = function (array, id){
     valorSeleccionado = array[id]
     print(dim(get(valorSeleccionado)))
-    python.load("/home/perez/tesis/source/image-generator-from-matrix.py")
+    pythonScript = toString(system.file(paste(SCRIPT_PATH,SCRIPT_FILE_NAME,sep=""), package = PACKAGE_NAME ))
+    print(pythonScript)
+    python.load(pythonScript)
     python.call("render_image", get(valorSeleccionado))
     load_image(imgfile = "/tmp/test.gif")
   }
 
-
   print("Visualizing!!!");
-  labelText <- tclVar("X:    Y:")
   black_and_white = c()
   for(i in ls(globalenv())){
     if("matrix" ==  class(get(i))){
       black_and_white <- c(black_and_white, i)
     }
   }
+
   win1 <- tktoplevel()
 
-  ##
+  ## MENU CONFIGURATION
   win1$env$menu <- tk2menu(win1)
   tkconfigure(win1, menu = win1$env$menu)
   win1$env$menuFile <- tk2menu(win1$env$menu, tearoff = FALSE)
@@ -36,6 +42,7 @@ rImageVisualizerVisualize = function(){
           command = function(data) { generating_image(black_and_white, 1)})
   }
 
+
   ##END BIG IF
   tkadd(win1$env$menuFile, "cascade", label = "Show matrix Black and White Image ",
         menu = win1$env$menuOpenRecent)
@@ -45,16 +52,17 @@ rImageVisualizerVisualize = function(){
   tkadd(win1$env$menu, "cascade", label = "File", menu = win1$env$menuFile)
 
   tktitle(win1) <- "Image Visualizer"
+
   win1$env$frm <- tk2frame(win1, borderwidth = 0, relief = "sunken",
                          padding = 0)
-  #tkpack(tk2label(win1,
-  #                textvariable = labelText,
-  #                width = 0, justify = "left", background = "#ffffff"),
-  #       side = "bottom", expand = FALSE, ipadx = 0, ipady = 0,
-  #       fill = "x")
-  #tkpack(win1$env$label)
 
-  load_image <- function(imgfile="/home/perez/tesis/images/lena.gif"){
+  tkpack(tk2label(win1,
+                  textvariable = labelText,
+                  width = 40, justify = "left", background = "#ffffff"),
+         side = "bottom", expand = FALSE, ipadx = 5, ipady = 5,
+         fill = "x")
+
+  load_image <- function(imgfile){
     image1 <- tclVar()
     tkimage.create("photo", image1, file = imgfile)
 
